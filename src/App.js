cee2826c00fcb718d7cf419bddaa532c7b9e7dd0
Ekohwo Ovie <preciousekohwo@gmail.com>
2024-09-05 22:28:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="top"></div>
+  <div id="second">
+  <ul class="second">
+    <li className="navfirst">
+      <a href="#welcome-section">Cakes</a>
+    </li>
+    <li id="navsecond">
+      <a href="#projects">Pastries</a>
+    </li>
+  </ul>
+  </div>
+    </div>
+  );
+}
+
+export default App;
